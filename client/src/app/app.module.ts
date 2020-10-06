6682239ef34970d71edba504d4b8febba7b371c3
mihnea9923 <54399158+mihnea9923@users.noreply.github.com>
2020-10-06 11:39:22 +0300

    identity API registration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
